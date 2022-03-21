09ea4468078981406f60305e16bda965897ab46a
sadahan <sarahdahan3012@gmail.com>
2022-03-21 12:36:27 +0100

    add change carac and neighbors

--- api.py
+++ api.py
@@ -2,9 +2,10 @@
 from flask import Flask, jsonify, request, make_response
 import joblib
 import pandas as pd, numpy as np
-# from lime.lime_tabular import LimeTabularExplainer
-# import matplotlib.pyplot as plt
-# import mpld3
+from lime.lime_tabular import LimeTabularExplainer
+import matplotlib.pyplot as plt
+# import sys
+import json
 
 app = Flask(__name__)
 
@@ -18,57 +19,66 @@ def predict():
     try:
         model = joblib.load("model/model.jbl")
         sample = pd.read_csv('data/sample.csv', index_col=0)
+        scaler = joblib.load("model/scaler.jbl")
     except:
         result = "Files couldn't be loaded."
         return make_response(result, 400)
     
     try:
-        id_customer = int(request.args.get('id_customer'))
-        customer = np.array(sample.loc[id_customer]).reshape(1, -1)
+        data_customer = request.args.get('data_customer')
+        data = pd.DataFrame(json.loads(data_customer), index=[0])
+        num_cols = [col for col in sample if not np.isin(sample[col].unique(), [0, 1]).all()]
+        # print(len(num_cols), file=sys.stderr)
+        data[num_cols] = scaler.transform(data[num_cols].values)
+        data = data.iloc[0]
+        customer = np.array(data).reshape(1, -1)
     except:
         result = 'Please enter a valid customer id.'
         return make_response(result, 400)
 
-    proba = model.predict_proba(customer)
-    pred = int(model.predict(customer)[0])
-    proba_0 = str(round(proba[0][0]*100,1)) + '%'
-    proba_1 = str(round(proba[0][1]*100,1)) + '%'
-    result = {'pred': pred,
-              'proba_0': proba_0,
-              'proba_1': proba_1}
-    return jsonify(result)
+    try:
+        proba = model.predict_proba(customer)
+        pred = int(model.predict(customer)[0])
+        proba_0 = str(round(proba[0][0]*100,1)) + '%'
+        proba_1 = str(round(proba[0][1]*100,1)) + '%'
+        result = {'pred': pred,
+                'proba_0': proba_0,
+                'proba_1': proba_1}
+        return jsonify(result)
+    except:
+        return jsonify(data_customer)
 
 
-# @app.route('/lime', methods=['GET', 'POST'])
-# def lime_explainer():
-#     try:
-#         model = joblib.load("model/model.jbl")
-#         sample = pd.read_csv('data/sample.csv', index_col=0)
-#     except:
-#         result = "Files couldn't be loaded."
-#         return make_response(result, 400)
+@app.route('/lime', methods=['GET', 'POST'])
+def lime_explainer():
+    try:
+        model = joblib.load("model/model.jbl")
+        sample = pd.read_csv('data/sample.csv', index_col=0)
+        scaler = joblib.load("model/scaler.jbl")
+    except:
+        result = "Files couldn't be loaded."
+        return make_response(result, 400)
 
-#     try:
-#         id_customer = int(request.args.get('id_customer'))
-#         customer = sample.loc[id_customer]
-#     except:
-#         result = 'Please enter a valid customer id.'
-#         return make_response(result, 400)
+    try:
+        data_customer = request.args.get('data_customer')
+        data = pd.DataFrame(json.loads(data_customer), index=[0])
+        num_cols = [col for col in sample if not np.isin(sample[col].unique(), [0, 1]).all()]
+        data[num_cols] = scaler.transform(data[num_cols].values)
+        sample[num_cols] = scaler.transform(sample[num_cols].values)
+        customer = data.iloc[0]
+    except:
+        result = 'Please enter a valid customer id.'
+        return make_response(result, 400)
     
-#     lime = LimeTabularExplainer(sample,
-#                                 feature_names=sample.columns,
-#                                 class_names=["Solvent", "Not Solvent"],
-#                                 discretize_continuous=False)
-#     exp = lime.explain_instance(customer,
-#                                 model.predict_proba,
-#                                 num_samples=100)
+    lime = LimeTabularExplainer(sample,
+                                feature_names=sample.columns,
+                                class_names=["Solvent", "Not Solvent"],
+                                discretize_continuous=False)
+    exp = lime.explain_instance(customer,
+                                model.predict_proba,
+                                num_samples=100)
 
-#     # exp.show_in_notebook(show_table=True)
-#     fig = exp.as_pyplot_figure()
-#     plt.tight_layout()
-
-#     return jsonify(fig.as_html())
-#     # return jsonify(mpld3.fig_to_html(fig))
+    return jsonify(exp.as_html())
 
 
 if __name__ == "__main__":
--- dashboard.py
+++ dashboard.py
@@ -3,58 +3,81 @@ import streamlit as st
 import streamlit.components.v1 as components
 import requests
 import joblib
-from lime.lime_tabular import LimeTabularExplainer
 import matplotlib.pyplot as plt
 import shap
 import seaborn as sns
-from PIL import Image
+from lime.lime_tabular import LimeTabularExplainer
+import json
+from sklearn.neighbors import NearestNeighbors
 
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 
-def request_prediction(id_customer):
+def request_prediction(data_customer):
     API_URL = 'http://127.0.0.1:5000/predict'
 
-    data_json = {'id_customer': id_customer}
+    data_json = {'data_customer': data_customer}
     response = requests.get(API_URL, params=data_json)
 
     if response.status_code != 200:
         raise Exception(
             "Request failed with status {}, {}".format(response.status_code, response.text))
+    return response.json()
 
+# def lime_explainer(data_customer):
+#     data = pd.DataFrame(json.loads(data_customer), index=[0])
+#     data[st.session_state.num_cols] = st.session_state.scaler.transform(data[st.session_state.num_cols].values)
+#     customer = data.iloc[0]
+
+#     lime = LimeTabularExplainer(st.session_state.sample,
+#                                 feature_names=st.session_state.sample.columns,
+#                                 class_names=["Solvent", "Not Solvent"],
+#                                 discretize_continuous=False)
+#     exp = lime.explain_instance(customer,
+#                                 st.session_state.model.predict_proba,
+#                                 num_samples=100)
+#     # exp.show_in_notebook(show_table=True)
+#     fig = exp.as_pyplot_figure()
+#     st.pyplot(fig)
+#     return exp.as_html()
+
+def lime_explainer(data_customer):
+    API_URL = 'http://127.0.0.1:5000/lime'
+
+    data_json = {'data_customer': data_customer}
+    response = requests.get(API_URL, params=data_json)
+
+    if response.status_code != 200:
+        raise Exception(
+            "Request failed with status {}, {}".format(response.status_code, response.text))
     return response.json()
 
 
-def lime_explainer(id_customer):
-    customer = st.session_state.sample.loc[id_customer]
-
-    lime = LimeTabularExplainer(st.session_state.sample,
-                                feature_names=st.session_state.sample.columns,
-                                class_names=["Solvent", "Not Solvent"],
-                                discretize_continuous=False)
-    exp = lime.explain_instance(customer,
-                                st.session_state.model.predict_proba,
-                                num_samples=100)
-    exp.show_in_notebook(show_table=True)
-    fig = exp.as_pyplot_figure()
-    st.pyplot(fig)
-    return exp.as_html()
-
-
-def shap_explainer(id_customer):
-    customer = st.session_state.sample.loc[id_customer]
-    i = st.session_state.sample.index.get_loc(id_customer)
+def shap_explainer(data_customer):
+    shap_sample = st.session_state.sample.copy()
+    customer = pd.Series(json.loads(data_customer))
+    shap_sample = shap_sample.append(customer, ignore_index=True)
+    i = shap_sample.shape[0] - 1
     explainer = shap.TreeExplainer(st.session_state.model)
-    shap_values = explainer.shap_values(st.session_state.sample)
+    shap_values = explainer.shap_values(shap_sample)
     st.pyplot(shap.force_plot(matplotlib=True,
                               base_value=explainer.expected_value[1],
                               shap_values=shap_values[1][i],
-                              features=st.session_state.sample.loc[id_customer],
-                              feature_names=st.session_state.sample.columns
+                              features=customer,
+                              feature_names=shap_sample.columns
                              ))
     
- 
+def get_closest_neighbors(df, id_customer, n_neighbors=5):
+    nn_model = NearestNeighbors(n_neighbors=n_neighbors+1)
+    nbrs = nn_model.fit(df)
+    list_neighbors = nn_model.kneighbors([df.loc[id_customer]])[1].flatten()
+    neighbors_ids = []
+    for n in list_neighbors:
+        neighbors_ids.append(df.iloc[n].name)
+    return neighbors_ids[1:]
+
+
 def display_importances(nb_feats):
     feature_importance_values = st.session_state.model.feature_importances_
 
@@ -75,13 +98,20 @@ def main():
     try:
         model = joblib.load("model/model.jbl")
         sample = pd.read_csv('data/sample.csv', index_col=0)
+        scaler = joblib.load("model/scaler.jbl")
     except:
         st.error("Files couldn't be loaded.")
-    
-    st.session_state.sample = sample
-    st.session_state.model = model
 
-    customers = sample.index.tolist() + [0]
+    num_cols = [col for col in sample if not np.isin(sample[col].unique(), [0, 1]).all()]
+    unscaled_sample = sample.copy()
+    sample[num_cols] = scaler.transform(sample[num_cols].values)
+
+    st.session_state.sample = sample
+    st.session_state.num_cols = num_cols
+    st.session_state.model = model
+    st.session_state.scaler = scaler
+
+    customers = sample.index.tolist()
     customers = list(map(str, sorted(customers)))
 
     st.title('Credit scoring prediction:')
@@ -91,32 +121,63 @@ def main():
                             min_value=5, max_value = 40, value=10, step=1)
     
     display_importances(nb_feats)
-    # col1, col2 = st.columns(2)
 
-    # with col1:
-    #     with st.form(key='form'):
     id_customer = st.selectbox('Please select a customer id:',
                                 customers)
     id_customer = int(id_customer)
-    # predict_btn = st.form_submit_button('Score')
+
+    customer = unscaled_sample.loc[id_customer]
+
+    c1, c2 = st.columns(2)
+    c1.number_input('Income of the client (AMT_INCOME_TOTAL) :',
+                    min_value=0.0, value=customer['AMT_INCOME_TOTAL'],
+                    key='income')
+    c2.number_input('Credit amount of the loan (AMT_CREDIT) :',
+                    min_value=0.0, value=customer['AMT_CREDIT'],
+                    key='credit')
+    c1.number_input('Loan annuity (AMT_ANNUITY) :',
+                    min_value=0.0, value=customer['AMT_ANNUITY'],
+                    key='annuity')
+    c2.number_input('Price of the goods for which the loan is given (AMT_GOODS_PRICE) :',
+                    min_value=0.0, value=customer['AMT_GOODS_PRICE'],
+                    key='goods_price')
+    nb_neighbors = c1.number_input('Number of similar clients to show :',
+                                    min_value=0, value=5)
+
+    customer['AMT_INCOME_TOTAL'] = st.session_state.income
+    customer['AMT_CREDIT'] = st.session_state.credit
+    customer['AMT_ANNUITY'] = st.session_state.annuity
+    customer['AMT_GOODS_PRICE'] = st.session_state.goods_price
+    customer['INCOME_CREDIT_PERC'] = customer['AMT_INCOME_TOTAL'] / customer['AMT_CREDIT']
+    customer['INCOME_PER_PERSON'] = customer['AMT_INCOME_TOTAL'] / customer['CNT_FAM_MEMBERS']
+    customer['ANNUITY_INCOME_PERC'] = customer['AMT_ANNUITY'] / customer['AMT_INCOME_TOTAL']
+    customer['PAYMENT_RATE'] = customer['AMT_ANNUITY'] / customer['AMT_CREDIT']
+
+    data_customer = customer.to_json()
+    pred = ~model.predict(sample).astype(bool)
+    unscaled_sample['solvent'] = pred
+
     predict_btn = st.button('Score')
 
     if predict_btn:
-        pred = request_prediction(id_customer)
+        pred = request_prediction(data_customer)
         if pred['pred'] == 0:
-            st.success('The customer is solvent ' + \
-                    'wih a probability of ' + pred['proba_0'])
+            st.success('The customer is solvent wih a probability of ' + pred['proba_0'])
         else:
-            st.error('The customer is not solvent ' + \
-                    'wih a probability of ' + pred['proba_1'])
+            st.error('The customer is not solvent wih a probability of ' + pred['proba_1'])
     
-        explainer = lime_explainer(id_customer)
-        shap_explainer(id_customer)
-                # json2html.convert(components.html(explainer, height=800))
+        explainer = lime_explainer(data_customer)
+        shap_explainer(data_customer)
         components.html(explainer, height=800)
-                # components.html("<html><body><h1>NO</h1></body></html>", width=200, height=200)
-    
 
+        neighbors = get_closest_neighbors(sample, id_customer, nb_neighbors)
+        cols = ['AMT_INCOME_TOTAL', 'AMT_CREDIT', 'AMT_ANNUITY', 'AMT_GOODS_PRICE',
+                'solvent']
+        df_neighbors = unscaled_sample[cols].rename(columns={'AMT_INCOME_TOTAL': 'income',
+         'AMT_CREDIT': 'credit', 'AMT_ANNUITY': 'annuity', 'AMT_GOODS_PRICE': 'goods_price'})
+        st.write('Features of ' + str(nb_neighbors) + ' similar customers:')
+        st.write(df_neighbors.loc[neighbors])
+    
 
 if __name__ == '__main__':
     main()
